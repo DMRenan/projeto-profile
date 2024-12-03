10bf9be271ab6756ae45f075c6d2ee276304f989
Renan Duarte <rnnmrcdrt@gmail.com>
2024-12-03 19:02:15 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Renan Duarte utilizando óculos de sol, sem camisa, com uma lata de cheque-mate na mão e uma camisa enrolada e amassada sobre um dos ombros."
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Renan Duarte utilizando óculos e vestindo jaqueta de cor azul clara aberta sobre uma camisa branca."
+    )
+  }
+}
